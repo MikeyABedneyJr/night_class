6fd68650d4d99fd7b7ec3929b8e75315573092f6
Mikey <mike.bedney@gmail.com>
2015-01-30 20:06:11 -0800

    Fixed issues: continues to run till user exits & has better display for adding user

--- Class3/PhoneBook.py
+++ Class3/PhoneBook.py
@@ -1,22 +1,15 @@
 phone_book = {'Frodo': {'Name' : 'Frodo', 'Number' : '919-999-1111'},
 				'Samwise': {'Name' : 'Samwise', 'Number' : '818-888-2222'}
 			}
-# print "-"*75
-# print "1. Look someone up"
-# print "2. Add new entry"
-# print "3. Modify entry"
-# print "4. Remove an entry"
-# print "5. View the directory"
-# print "9. Exit this AMAZING program"
-# print "-"*75
-# choice = raw_input("What do you want to do?: ")
-# execute_menu(choice)
 
 
+run = True
 
-
-
-def execute_menu():
+while run:
+	print "-"*75
+	print "1. Look someone up\n2. Add new entry\n3. Modify entry\n4. Remove an entry\n5. View the directory\n9. Exit this AMAZING program\n"
+	print "-"*75
+	choice = raw_input("What do you want to do?: ")
 	if (choice == "1"):
 		print "You've chosen to look someone up..."
 		Who = raw_input("Who do you want to look up?: ")
@@ -24,15 +17,17 @@ def execute_menu():
 		print phone_book[Who]['Name']
 		print "Their number is:"
 		print phone_book[Who]['Number']
-		main_menu()
 
 	if (choice == "2"):
 		print "You've chosen to add an entry..."
 		AddUserName = raw_input("What is the name of your new entry?: ")
 		AddUserNum = raw_input("What is this person's phone number?: ")
 		phone_book.update({AddUserName: {'Name' : AddUserName, 'Number' : AddUserNum}})
-		print phone_book
-		main_menu()
+		print "-"*75
+		print "\n\n\n\n\nCurrent phone book entries:\n"
+		for i in phone_book:
+			for j in phone_book[i]:
+				print(phone_book[i][j])
 
 	if (choice == "3"):
 		print "You've chosen to modify an entry..."
@@ -44,13 +39,12 @@ def execute_menu():
 			ChangeName = raw_input("What is the new name?: ")
 			phone_book[NameMod]['Name'] = ChangeName
 			print phone_book
-			main_menu()
+
 		else:
 			print "You've chosen to change the number..."
 			ChangeNum = raw_input("What is the new number?: ")
 			phone_book[NameMod]['Number'] = ChangeNum
 			print phone_book
-			main_menu()
 
 	if (choice == "4"):
 		print "You've chosen to remove an entry..."
@@ -58,26 +52,12 @@ def execute_menu():
 		del phone_book[NameToRemove]
 		print "Your phonebook is now: "
 		print phone_book
-		main_menu()
 
 	if (choice == "5"):
 		print "Here is the current directory: "
 		print phone_book['Name']
-		main_menu()
 
 	if (choice =="9"):
 		print "Thanks for.....playing? :)"
+		run = False
 
-def main_menu():
-	print "-"*75
-	print "1. Look someone up"
-	print "2. Add new entry"
-	print "3. Modify entry"
-	print "4. Remove an entry"
-	print "5. View the directory"
-	print "9. Exit this AMAZING program"
-	print "-"*75
-	choice = raw_input("What do you want to do?: ")
-	execute_menu(choice)
-
-main_menu()
