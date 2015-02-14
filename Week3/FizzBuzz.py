8a800bf7b207bff931b57f267d6497cfa5edaefc
unknown <mike.bedney@sma-laptop25.stmaryspdx.net>
2015-02-13 19:37:34 -0800

    Updated FizzBuzz for cleaner code after suggestion from person from Twitter :-)

--- Week3/FizzBuzz.py
+++ Week3/FizzBuzz.py
@@ -7,13 +7,10 @@ count = 0
 while count < 101:
 	if (count % 3 == 0) and (count % 5 == 0):
 		print "FizzBuzz"
-		count += 1
 	elif (count % 3 == 0):
 		print "Fizz"
-		count += 1
 	elif (count % 5 == 0):
 		print "Buzz"
-		count += 1
 	else:
 		print count
-		count += 1
+	count += 1
